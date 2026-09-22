1de25d401e067ce1d9d04d6fc7c71866e2f71e31
cheton_wu <cheton_wu@trendmicro.com>
2026-09-22 17:04:36 +0800

    refactor(visualizer): use Tonic workflow toast

--- src/app/widgets/Visualizer/Notifications.js
+++ src/app/widgets/Visualizer/Notifications.js
@@ -1,11 +1,11 @@
 import {
   Box,
+  Link,
   Space,
+  Text,
+  Toast,
 } from '@tonic-ui/react';
-import PropTypes from 'prop-types';
 import React from 'react';
-import Anchor from '@app/components/Anchor';
-import { ToastNotification } from '@app/components/Notifications';
 import i18n from '@app/lib/i18n';
 import {
   NOTIFICATION_PROGRAM_ERROR,
@@ -18,7 +18,27 @@ import {
   NOTIFICATION_M190_SET_HEATED_BED_TEMPERATURE
 } from './constants';
 
-function Notifications({ show, type, data, onDismiss, ...props }) {
+const noop = () => {};
+
+/**
+ * @param {{
+ *   show?: boolean,
+ *   type?: string,
+ *   data?: unknown,
+ *   onDismiss?: () => void,
+ * }} props Component props.
+ */
+function Notifications({
+  show = true,
+  type = '',
+  data = null,
+  onDismiss = noop,
+  ...props
+}) {
+  if (!show) {
+    return null;
+  }
+
   return (
     <Box
       {...props}
@@ -27,10 +47,9 @@ function Notifications({ show, type, data, onDismiss, ...props }) {
       left={0}
       right={0}
     >
-      <ToastNotification
+      <Toast
         {...props}
-        show={show}
-        type={{
+        appearance={{
           [NOTIFICATION_PROGRAM_ERROR]: 'error',
           [NOTIFICATION_M0_PROGRAM_PAUSE]: 'info',
           [NOTIFICATION_M1_PROGRAM_PAUSE]: 'info',
@@ -40,99 +59,93 @@ function Notifications({ show, type, data, onDismiss, ...props }) {
           [NOTIFICATION_M109_SET_EXTRUDER_TEMPERATURE]: 'warning',
           [NOTIFICATION_M190_SET_HEATED_BED_TEMPERATURE]: 'warning'
         }[type]}
-        onDismiss={onDismiss}
+        isClosable
+        onClose={onDismiss}
       >
         {type === NOTIFICATION_PROGRAM_ERROR && (
-          <div>
-            <div><strong>{i18n._('Error')}</strong></div>
+          <Box>
+            <Box><Text as="strong">{i18n._('Error')}</Text></Box>
             {data && (
-              <code>{data}</code>
+              <Box as="code">{data}</Box>
             )}
-            <div>{i18n._('Click the Resume button to resume program execution.')}</div>
-          </div>
+            <Box>{i18n._('Click the Resume button to resume program execution.')}</Box>
+          </Box>
         )}
         {type === NOTIFICATION_M0_PROGRAM_PAUSE && (
-          <div>
-            <div><strong>{i18n._('M0 Program Pause')}</strong></div>
+          <Box>
+            <Box><Text as="strong">{i18n._('M0 Program Pause')}</Text></Box>
             {data && (
-              <code>{data}</code>
+              <Box as="code">{data}</Box>
             )}
-            <div>{i18n._('Click the Resume button to resume program execution.')}</div>
-          </div>
+            <Box>{i18n._('Click the Resume button to resume program execution.')}</Box>
+          </Box>
         )}
         {type === NOTIFICATION_M1_PROGRAM_PAUSE && (
-          <div>
-            <div><strong>{i18n._('M1 Program Pause')}</strong></div>
+          <Box>
+            <Box><Text as="strong">{i18n._('M1 Program Pause')}</Text></Box>
             {data && (
-              <code>{data}</code>
+              <Box as="code">{data}</Box>
             )}
-            <div>{i18n._('Click the Resume button to resume program execution.')}</div>
-          </div>
+            <Box>{i18n._('Click the Resume button to resume program execution.')}</Box>
+          </Box>
         )}
         {type === NOTIFICATION_M2_PROGRAM_END && (
-          <div>
-            <div><strong>{i18n._('M2 Program End')}</strong></div>
+          <Box>
+            <Box><Text as="strong">{i18n._('M2 Program End')}</Text></Box>
             {data && (
-              <code>{data}</code>
+              <Box as="code">{data}</Box>
             )}
-            <div>{i18n._('Click the Stop button to stop program execution.')}</div>
-          </div>
+            <Box>{i18n._('Click the Stop button to stop program execution.')}</Box>
+          </Box>
         )}
         {type === NOTIFICATION_M30_PROGRAM_END && (
-          <div>
-            <div><strong>{i18n._('M30 Program End')}</strong></div>
+          <Box>
+            <Box><Text as="strong">{i18n._('M30 Program End')}</Text></Box>
             {data && (
-              <code>{data}</code>
+              <Box as="code">{data}</Box>
             )}
-            <div>{i18n._('Click the Stop button to stop program execution.')}</div>
-          </div>
+            <Box>{i18n._('Click the Stop button to stop program execution.')}</Box>
+          </Box>
         )}
         {type === NOTIFICATION_M6_TOOL_CHANGE && (
-          <div>
-            <div><strong>{i18n._('M6 Tool Change')}</strong></div>
+          <Box>
+            <Box><Text as="strong">{i18n._('M6 Tool Change')}</Text></Box>
             {data && (
-              <code>{data}</code>
+              <Box as="code">{data}</Box>
             )}
-            <div>
+            <Box>
               {i18n._('Run a tool change macro to change the tool and adjust the Z-axis offset. Afterwards, click the Resume button to resume program execution.')}
               <Space width={8} />
-              <Anchor
+              <Link
                 target="_blank"
                 href="https://github.com/cncjs/cncjs/wiki/Tool-Change"
               >
                 {i18n._('Learn more')}
-              </Anchor>
-            </div>
-          </div>
+              </Link>
+            </Box>
+          </Box>
         )}
         {type === NOTIFICATION_M109_SET_EXTRUDER_TEMPERATURE && (
-          <div>
-            <div><strong>{i18n._('M109 Set Extruder Temperature')}</strong></div>
+          <Box>
+            <Box><Text as="strong">{i18n._('M109 Set Extruder Temperature')}</Text></Box>
             {data && (
-              <code>{data}</code>
+              <Box as="code">{data}</Box>
             )}
-            <div>{i18n._('Waiting for the target temperature to be reached...')}</div>
-          </div>
+            <Box>{i18n._('Waiting for the target temperature to be reached...')}</Box>
+          </Box>
         )}
         {type === NOTIFICATION_M190_SET_HEATED_BED_TEMPERATURE && (
-          <div>
-            <div><strong>{i18n._('M190 Set Heated Bed Temperature')}</strong></div>
+          <Box>
+            <Box><Text as="strong">{i18n._('M190 Set Heated Bed Temperature')}</Text></Box>
             {data && (
-              <code>{data}</code>
+              <Box as="code">{data}</Box>
             )}
-            <div>{i18n._('Waiting for the target temperature to be reached...')}</div>
-          </div>
+            <Box>{i18n._('Waiting for the target temperature to be reached...')}</Box>
+          </Box>
         )}
-      </ToastNotification>
+      </Toast>
     </Box>
   );
 }
-
-Notifications.propTypes = {
-  show: PropTypes.bool,
-  type: PropTypes.string,
-  data: PropTypes.any,
-  onDismiss: PropTypes.func
-};
 
 export default Notifications;
